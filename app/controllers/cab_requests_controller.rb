2b999e67820a388fb4aa128f6417ce971cbfd5c0
apurvagupta <apurvagupta0@gmail.com>
2013-02-07 15:38:09 +0530

    <Apurva/Priyanka/Vishal> Create view story, added dropdown list for destination, source, number of passenger

--- app/controllers/cab_requests_controller.rb
+++ app/controllers/cab_requests_controller.rb
@@ -1,20 +1,16 @@
 class CabRequestsController < ApplicationController
 
   def logout
-
     cookies.delete :auth_token
     reset_session
     flash[:notice] = "You have been logged out."
     CASClient::Frameworks::Rails::Filter.logout(self)
-
   end
+
   def index
-
     @cab_request = CabRequest.new
+    @num = Array(1..50)
+    @sources = ["Airport","Guest House","McKinsey","ThoughtWorks"]
+    @destinations = ["Airport","Guest House","McKinsey","ThoughtWorks"]
   end
-
-
-
 end
-
-
---
+++ spec/controllers/cab_requests_controller_spec.rb
@@ -0,0 +1,10 @@
+require "test/spec"
+
+context "index" do
+
+  it "should do " do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
