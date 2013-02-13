1ea3bf08ea02e8bfc06edbb42213405aba2da5b3
apurvagupta <apurvagupta0@gmail.com>
2013-02-13 11:28:03 +0530

    <vishal/apurva>13-02-2013 model's test done

--- spec/models/cab_request_spec.rb
+++ spec/models/cab_request_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe CabRequest do
 
   before :each do
-    @cab_request = CabRequest.new :requester=> "Apurva",:pick_up_for=>"Vishal", :contact_no => "8527644166" , :date => "2013-08-27", :time => "05:30" ,:source=>"dilshad garden",:destination=>"office",:no_of_passengers=>3, :comments => "Please"
+    @cab_request = CabRequest.new :requester=> "Apurva",:pick_up_for=>"Vishal", :contact_no => "8527644166" , :date => "2013-08-27", :pick_up_time => "05:30" ,:source=>"dilshad garden",:destination=>"office",:no_of_passengers=>3, :comments => "Please"
   end
 
   context "Class structure validations" do
@@ -13,7 +13,7 @@ describe CabRequest do
       (@cab_request.has_attribute? :pick_up_for).should be_true
       (@cab_request.has_attribute? :contact_no).should be_true
       (@cab_request.has_attribute? :date).should be_true
-      (@cab_request.has_attribute? :time).should be_true
+      (@cab_request.has_attribute? :pick_up_time).should be_true
       (@cab_request.has_attribute? :source).should be_true
       (@cab_request.has_attribute? :destination).should be_true
       (@cab_request.has_attribute? :no_of_passengers).should be_true
@@ -76,7 +76,7 @@ describe CabRequest do
     end
 
     it "should not contain more or less than ten digits" do
-      @cab_request.contact_no = "45678"
+      @cab_request.contact_no = "456780"
       @cab_request.save.should be_false
       @cab_request.errors[:contact_no].first.should == "is the wrong length (should be 10 characters)"
     end
@@ -91,11 +91,11 @@ describe CabRequest do
     end
   end
 
-  context "Time" do
+  context "pick_up_time" do
     it "should not be blank" do
-      @cab_request.time = nil
+      @cab_request.pick_up_time = nil
       @cab_request.save.should be_false
-      @cab_request.errors[:time].first.should == "can't be blank"
+      @cab_request.errors[:pick_up_time].first.should == "can't be blank"
     end
 
   end
@@ -106,6 +106,12 @@ describe CabRequest do
       @cab_request.source = nil
       @cab_request.save.should be_false
       @cab_request.errors[:source].first.should == "can't be blank"
+    end
+
+    it "Should not be same" do
+      @cab_request.destination=@cab_request.source
+      @cab_request.save.should be_false
+      @cab_request.errors[:source].first.should== " and Destination can't be same"
     end
 
   end
@@ -138,5 +144,6 @@ describe CabRequest do
       @cab_request.errors[:comments].first.should == "is too long (maximum is 25 characters)"
     end
   end
+
 
 end
