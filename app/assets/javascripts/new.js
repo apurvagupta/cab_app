861a47087f898f087585097fab42e41d716046c2
Vishalsh <sharvishi9118@gmail.com>
2013-02-14 12:41:36 +0530

    <riya/vishal>statring with client side validations

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,4 +1,5 @@
 
+
 function onSelectedValueChange(ddl_id,txt_id)
 {
     var ddl_list = document.getElementById(ddl_id);
@@ -12,4 +13,20 @@ function onSelectedValueChange(ddl_id,txt_id)
         document.getElementById(txt_id).hidden = true;
         document.getElementById(txt_id).disabled=true;
     }
+}
+
+function my_func(puf,l)
+{
+      var x=document.getElementById(puf).value;
+        if (x==null || x=="")
+        {
+            document.getElementById(l).hidden = false;
+            window.setTimeout(function(){document.getElementById(puf).focus();},10)
+        }
+        else
+        {
+            document.getElementById(l).hidden = true;
+
+        }
+
 }
