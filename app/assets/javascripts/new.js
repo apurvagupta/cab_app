57cfc3e27dc4617fa49dfb80ea7e3c99fc8debe8
Pulkit <pulkit@INPULKIT.(none)>
2013-02-26 10:50:17 +0530

    <pulkit> hidden source & destination corrected

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -4,12 +4,13 @@ function onSelectedValueChange(ddl_id,txt_id)
     var ddl_list = document.getElementById(ddl_id);
     if  (ddl_list.value=="other")
     {
-        document.getElementById(txt_id).hidden = false;
+        document.getElementById(txt_id).type = "text";
         document.getElementById(txt_id).disabled=false;
+        document.getElementById(txt_id).focus()
     }
     else
     {
-        document.getElementById(txt_id).hidden = true;
+        document.getElementById(txt_id).type = "hidden";
         document.getElementById(txt_id).disabled=true;
     }
 
