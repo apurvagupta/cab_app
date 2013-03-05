51a2f9850916e531c6a4de232810b612d9785587
apurvagupta <apurvagupta0@gmail.com>
2013-03-05 09:59:44 +0530

    some file created

--- spec/controllers/cab_requests_controller_spec.rb
+++ spec/controllers/cab_requests_controller_spec.rb
@@ -8,6 +8,7 @@ describe CabRequestsController do
       @cab_request  = { requester: "Priyanka Pathak", traveler_name: "self", pick_up_date_time: Time.new(2913, 02, 07, 4, 0, 0, "+05:30"), contact_no: "9039409828", source: "Guest House", destination: "ThoughtWorks", no_of_passengers: 1, comments: "something" }
       CASClient::Frameworks::Rails::Filter.fake("homer")
       post :create, cab_request: @cab_request, pick_up_date: @pick_up_date
+
       response.should redirect_to ("/cab_requests/show")
     end
 
