f5f3c037880b0ad36c8395cfb8a31d1e3c49dafc
arnav90 <arnavk90@thoughtworks.com>
2013-03-11 17:50:28 +0530

    fixed routes for home page.

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ CabApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'requesters#index'
+   root :to => 'cab_requests#new'
 
   # See how all your routes lay out with "rake routes"
 
