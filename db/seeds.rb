5c61f864122a677d12c9bdc4c5898102e32ec58b
arnav90 <arnavk90@thoughtworks.com>
2013-03-11 17:51:38 +0530

    Added sample cab_request for controller tests.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 admin = Admin.create( name: "pulkitko", contact_no: "8527644166", status: true)
 vendor = Vendor.create( name: "gopal", contact_no: "9812345678", status: true)
+CabRequest.create!(requester: "homer", traveler_name: "self",pick_up_date_time: Time.now + 2.days,
+                  contact_no: "9039409828",source: "Guest House", destination: "ThoughtWorks",
+                  no_of_passengers: 1, comments: "something", pick_up_date: (Time.now + 2.days).to_date )
