2e50fef0fa2bf9ff38b7e7e2e887b4d059a241b2
arnavk90 <arnavk90@gmail.com>
2013-03-20 15:08:30 +0530

    +Priyanka Created Date Time parser to build pick_up_date_time.

--- app/controllers/cab_requests_controller.rb
+++ app/controllers/cab_requests_controller.rb
@@ -6,31 +6,34 @@ class CabRequestsController < ApplicationController
     @cab_request = CabRequest.new(requester: @req.requester_name, contact_no: @req.requester_contact_no)
   end
 
+  def date_time_parser(date,time)
+    unless date.nil? || time.nil?
+      DateTime.parse(date + ' ' + time + ' +05:30').strftime('%F %T %z')
+    end
+
+  end
+
   def create
     @cab_request                   = CabRequest.new(params[:cab_request])
     @cab_request.requester         = session[:cas_user]
-    @cab_request.pick_up_date_time = Time.new(params[:cab_request][:pick_up_date].to_date.year,
-                                              params[:cab_request][:pick_up_date].to_date.month,
-                                              params[:cab_request][:pick_up_date].to_date.day,
-                                              @cab_request.pick_up_date_time.hour,
-                                              @cab_request.pick_up_date_time.min,
-                                              @cab_request.pick_up_date_time.sec,
-                                              "+05:30") rescue nil
+
+    @cab_request.pick_up_date_time = date_time_parser(params[:cab_request][:pick_up_date],params[:cab_request][:pick_up_date_time])
+
     @other_source=params[:source]
     @other_destination=params[:destination]
 
-    if @cab_request.source=='other'
-      @cab_request.source=@other_source
+    if @cab_request.source == 'other'
+      @cab_request.source = @other_source
     end
-    if @cab_request.destination=='other'
-      @cab_request.destination=@other_destination
+    if @cab_request.destination == 'other'
+      @cab_request.destination = @other_destination
     end
 
     if @cab_request.save
       @notice = 'YOUR REQUEST HAS BEEN SENT'
       redirect_to '/cab_requests/show'
     else
-      render template: "cab_requests/new"
+      render template: 'cab_requests/new'
     end
   end
 
