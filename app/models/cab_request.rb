f93078e0eb4d8b3e46c3ec97d541ac146477c64c
arnavk90 <arnavk90@gmail.com>
2013-03-20 15:07:04 +0530

    +Priyanka Refactoring. Fixed syntax and removed rescue nil. WIP

--- app/models/cab_request.rb
+++ app/models/cab_request.rb
@@ -7,7 +7,7 @@ class CabRequest < ActiveRecord::Base
   validates_format_of :traveler_name, with: /^[a-z.A-Z\s]*$/
   validates_numericality_of :contact_no, only_integer: true
   validates_numericality_of :no_of_passengers
-  validates_inclusion_of :no_of_passengers, in: 1..50, message: "should not be more than 50"
+  validates_inclusion_of :no_of_passengers, in: 1..50, message: 'should not be more than 50'
   validates_length_of :contact_no, is: 10
   validates_length_of :comments, maximum: 25
   validates_length_of :traveler_name, maximum: 10
@@ -17,22 +17,15 @@ class CabRequest < ActiveRecord::Base
   validate :check_source_and_destination
   validate :time_validation
 
-  #def self.to_csv(options = {})
-  #  CSV.generate(options) do |csv|
-  #    csv << column_names
-  #    all.each do |cab_request|
-  #      csv << cab_request.attributes.values_at(*column_names)
-  #    end
-  #  end
-  #end
-
   def check_source_and_destination
-    errors.add(:source, " and Destination can't be same") if source == destination
+    errors.add(:source, ' and Destination can\'t be same') if source == destination
   end
 
   def time_validation
-    current_time = Time.now
-    errors.add(:pick_up_date_time,"should not be less than current time") if pick_up_date_time < current_time rescue nil
+    unless pick_up_date_time.nil?
+      current_time = Time.now
+      errors.add(:pick_up_date_time,'should not be less than current time') if pick_up_date_time < current_time
+    end
   end
 
 end
--- app/models/requester.rb
+++ app/models/requester.rb
@@ -4,28 +4,28 @@ class Requester
   attr_accessor :requester_name, :requester_contact_no
 
   def self.gab_link(user_id)
-    api = "https://my.thoughtworks.com/api/core/v2/users/username/" + user_id rescue nil
-    @source_code = open(api, http_basic_authentication: ["boogaBooga", "boogaBooga"]).read rescue nil
+    api = 'https://my.thoughtworks.com/api/core/v2/users/username/' + user_id rescue nil
+    @source_code = open(api, http_basic_authentication: ['boogaBooga', 'boogaBooga']).read rescue nil
   end
 
 
   def self.extract (field)
-    start_index = @source_code.index(field) + field.length + 5 rescue nil
-    extracted_source_code = @source_code[start_index, start_index+50] rescue nil
-    end_index = extracted_source_code.index('"') rescue nil
-    extracted_source_code = extracted_source_code[0, end_index] rescue nil
-    if extracted_source_code
-      return extracted_source_code.strip
+    unless @source_code.nil?
+      start_index = @source_code.index(field) + field.length + 5
+      extracted_source_code = @source_code[start_index, start_index+50]
+      end_index = extracted_source_code.index('"')
+      extracted_source_code = extracted_source_code[0, end_index]
+      extracted_source_code.strip
     end
-    return extracted_source_code
+
   end
 
   def self.fetch_requester_info(user_id)
     self.gab_link(user_id)
-    @req                      = Requester.new
-    @req.requester_name       = self.extract("name") || user_id
-    @req.requester_contact_no = self.extract("mobile")
-    return @req
+    req                      = Requester.new
+    req.requester_name       = self.extract('name') || user_id
+    req.requester_contact_no = self.extract('mobile')
+    req
   end
 
 end
