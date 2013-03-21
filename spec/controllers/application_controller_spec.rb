945a47f51e7df91ccd00858702f57d4f001526e1
arnavk90 <arnavk90@gmail.com>
2013-03-21 11:30:34 +0530

    +Priyanka Refactoring Test.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,34 +4,22 @@ describe ApplicationController do
   # This is the anonymous controller:
   controller do
     def new
-      render :text => "Hello"
+      render :text => 'Hello'
     end
   end
 
-  it "should redirect to CAS login page if user is not logged in" do
+  it 'should redirect to CAS login page if user is not logged in' do
     get :new
     if !session[:cas_sent_to_gateway].nil?
-      response.should redirect_to("https://cas.thoughtworks.com/cas/login?service=http%3A%2F%2Ftest.host%2Fanonymous%2Fnew")
+      response.should redirect_to('https://cas.thoughtworks.com/cas/login?service=http%3A%2F%2Ftest.host%2Fanonymous%2Fnew')
     end
   end
 
-  it "should redirect to new request page if user is logged in" do
-    CASClient::Frameworks::Rails::Filter.fake("homer")
+  it 'should redirect to new request page if user is logged in' do
+    CASClient::Frameworks::Rails::Filter.fake('homer')
     get :new
     response.should be_success
   end
 
-  it 'should allow admin permissions if the logged in user is admin' do
-    Admin.create(name: "homer", contact_no: "9876543212", status: false)
-    CASClient::Frameworks::Rails::Filter.fake("homer")
-    get :new
-    assigns(:is_admin).should == true
-  end
-
-  it 'should allow admin permissions if the logged in user is not admin' do
-    CASClient::Frameworks::Rails::Filter.fake("riyakata")
-    get :new
-    assigns(:is_admin).should == false
-  end
 end
 
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,12 +1,24 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
+
   it 'should convert time to IST correctly.'do
     original_time = Time.now.utc
-    expected_time = (original_time + 19800).strftime("%I:%M %P")
+    expected_time = (original_time + 19800).strftime('%I:%M %P')
     converted_time = ist(original_time)
-
     converted_time.should == expected_time
 
+  end
+
+  it 'should return true if the logged in user is admin' do
+    session[:cas_user] = 'homer'
+    Admin.create(name: 'homer', contact_no: '9876543212', status: false)
+    CASClient::Frameworks::Rails::Filter.fake('homer')
+    is_admin?.should == true
+  end
+
+  it 'should return false if the logged in user is not an admin' do
+    CASClient::Frameworks::Rails::Filter.fake('homer')
+    is_admin?.should == false
   end
 end
