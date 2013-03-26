09fe5b8068273ffd087d98f5f927bb67b2a4e764
arnavk90 <arnavk90@gmail.com>
2013-03-26 11:26:52 +0530

    +Priyanka Added factory for vendors.

---
+++ spec/factories/vendor_factory.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :vendor, class: Vendor do
+    name 'bhidu'
+    contact_no '9039499999'
+    status true
+  end
+
+  factory :inactive_valid_vendor, parent: :vendor do
+    status false
+  end
+
+  factory :invalid_vendor, parent: :vendor do
+    contact_no '9876543'
+  end
+end
