5fba498478655b0e41e67d8a75214e028c4124af
arnavk90 <arnavk90@gmail.com>
2013-03-26 11:27:26 +0530

    +Priyanka Updated vendor model and controller specs to use factory.

--- spec/controllers/vendors_controller_spec.rb
+++ spec/controllers/vendors_controller_spec.rb
@@ -3,15 +3,14 @@ require 'spec_helper'
 describe VendorsController do
   before :each do
     CASClient::Frameworks::Rails::Filter.fake('homer')
-    @vendor_hash = {name: 'sushil', contact_no: '1234567890', status:'true'}
-    @vendor = Vendor.create(@vendor_hash)
+    @active_valid_vendor = create(:vendor)
   end
 
   context 'Index' do
     it 'should populate array of vendor' do
       get :index
       response.should be_success
-      controller.instance_variable_get(:@vendors).should == [@vendor]
+      controller.instance_variable_get(:@vendors).should == [@active_valid_vendor]
     end
     it 'should renders the index view' do
       get :index
@@ -35,45 +34,41 @@ describe VendorsController do
 
   context 'Edit' do
     it 'should find a particular Vendor' do
-      get :edit ,id: @vendor.id
+      get :edit ,id: @active_valid_vendor.id
       response.should be_success
-      controller.instance_variable_get(:@info).should == @vendor
+      controller.instance_variable_get(:@info).should == @active_valid_vendor
     end
     it 'should render the edit view ' do
-      get :edit,id: @vendor.id
+      get :edit,id: @active_valid_vendor.id
       response.should render_template('vendors/edit')
     end
   end
 
   context 'create' do
     it 'should redirect to the index page when saved' do
-      new_vendor_hash = @vendor_hash.merge!(name: 'tina',status: false)
-      post :create ,vendor: new_vendor_hash
+      post :create ,vendor: attributes_for(:inactive_valid_vendor)
       response.should redirect_to(vendors_path)
     end
     it 'should render new when not saved' do
-      invalid_vendor_hash = @vendor_hash.merge(contact_no: '12345')
-      post :create ,vendor: invalid_vendor_hash
+      post :create ,vendor: attributes_for(:invalid_vendor)
       response.should render_template('vendors/new')
     end
   end
 
   context 'update' do
     it 'should redirect to index page when updated ' do
-      updated_vendor_hash = @vendor_hash.merge(status: false)
-      put :update, id: @vendor.id , vendor: updated_vendor_hash
+      put :update, id: @active_valid_vendor.id , vendor: attributes_for(:vendor, contact_no: '1234567890')
       response.should redirect_to(vendors_path)
     end
     it 'should render edit when not updated' do
-      invalid_update_vendor_hash = @vendor_hash.merge(contact_no: '12345',status: false)
-      put :update ,id: @vendor.id, vendor: invalid_update_vendor_hash
+      put :update ,id: @active_valid_vendor.id, vendor: attributes_for(:invalid_vendor)
       response.should render_template('vendors/edit')
     end
   end
 
   context 'destroy' do
     it 'should redirect to index page when deleted ' do
-      delete :destroy, id: @vendor.id
+      delete :destroy, id: @active_valid_vendor.id
       response.should redirect_to(vendors_path)
     end
   end
--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -3,67 +3,67 @@ require 'spec_helper'
 describe Vendor do
 
   before :each do
-    @vendor = Vendor.new(name: "sample", contact_no: "1234567890", status: false)
+    @vendor = build(:vendor)
   end
 
-  context "Class structure validations" do
+  context 'Class structure validations' do
 
-    it "should have all required fields  " do
+    it 'should have all required fields  ' do
       (@vendor.has_attribute? :name).should be_true
       (@vendor.has_attribute? :contact_no).should be_true
     end
 
   end
 
-  context "all mandatory fields" do
+  context 'all mandatory fields' do
 
-    it "should contain a value" do
+    it 'should contain a value' do
       @vendor.save.should be_true
     end
 
   end
 
-  context "vendor name" do
+  context 'vendor name' do
 
-    it "should not be blank" do
+    it 'should not be blank' do
       @vendor.name= nil
       @vendor.save.should be_false
     end
 
-    it "should not contain digits" do
-      @vendor.name= "hello@123"
+    it 'should not contain digits' do
+      @vendor.name= 'hello@123'
       @vendor.save.should be_false
     end
 
   end
 
-  context "vendor contact number " do
+  context 'vendor contact number ' do
 
-    it "should not be blank" do
+    it 'should not be blank' do
       @vendor.contact_no = nil
       @vendor.save.should be_false
-      @vendor.errors[:contact_no].first.should == "can't be blank"
+      @vendor.errors[:contact_no].first.should == 'can\'t be blank'
     end
 
-    it "should not contain non-numeric data" do
-      @vendor.contact_no = "a$$@7466748"
+    it ' should not contain non-numeric data ' do
+      @vendor.contact_no = ' a $$ @ 7466748 '
       @vendor.save.should be_false
-      @vendor.errors[:contact_no].first.should == "is not a number"
+      @vendor.errors[:contact_no].first.should == 'is not a number'
     end
 
-    it "should not contain more or less than ten digits" do
-      @vendor.contact_no = "45678"
+    it ' should not contain more or less than ten digits ' do
+      @vendor.contact_no = '45678'
       @vendor.save.should be_false
-      @vendor.errors[:contact_no].first.should == "is the wrong length (should be 10 characters)"
+      @vendor.errors[:contact_no].first.should == 'is the wrong length (should be 10 characters)'
     end
 
   end
 
-  context "Vendor Status " do
-    it "should not contain other than a boolean value " do
+  context ' Vendor Status ' do
+    it ' should not contain other than a boolean value ' do
       @vendor.status = nil
       @vendor.save.should be_false
-      @vendor.errors[:status].first.should == "is not included in the list"
+      @vendor.errors[:status].first.should == 'is not included in the list'
     end
   end
 
