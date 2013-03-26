c75574700e39719e2374ca18a0d3dfe3351862d1
arnavk90 <arnavk90@gmail.com>
2013-03-26 10:33:43 +0530

    +Priyanka Updated admin model and controller specs to use factories.

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -2,77 +2,77 @@ require 'spec_helper'
 
 describe AdminsController do
   before :each do
-    CASClient::Frameworks::Rails::Filter.fake("homer")
-    @sample_admin = Admin.create(name: 'pulkitko', contact_no: "8765432100", status: true)
-    @sample_admin2 = Admin.create(name: 'ppathak', contact_no: "8765432100", status: false)
-    @sample_admins = [@sample_admin,@sample_admin2]
+    CASClient::Frameworks::Rails::Filter.fake('homer')
+    @sample_admin = create(:valid_admin_active)
+    @sample_admin2 = create(:valid_admin_inactive)
+    @sample_admins = [@sample_admin, @sample_admin2]
   end
   context 'index' do
     it 'should get list of admins' do
       get :index
-      assigns(:admins).should == @sample_admins
+      controller.instance_variable_get(:@admins).should == @sample_admins
     end
   end
 
   context 'new' do
     it 'should create empty Admin object' do
-      admin2=Admin.new
-      Admin.should_receive(:new).and_return(admin2)
+      empty_admin_object = Admin.new
+      Admin.should_receive(:new).and_return(empty_admin_object)
       get :new
-      assigns(:info).should == admin2
+      controller.instance_variable_get(:@info).should == empty_admin_object
     end
   end
 
   context 'edit' do
     it 'should get the correct admin' do
       get :edit, id: @sample_admin.id
-      assigns(:info).should == @sample_admin
+      controller.instance_variable_get(:@info).should == @sample_admin
     end
   end
 
   context 'create' do
     it 'should redirect to admins_path on successful save' do
-      new_admin = {name: 'pulkitko', contact_no: "8765432100", status: false}
+      new_admin = attributes_for(:valid_admin_inactive)
       post :create, admin: new_admin
       response.should redirect_to(admins_path)
     end
 
-   it 'should render new on failed save' do
-      new_admin = {name: 'pulkitko', contact_no: "876543210", status: false}
+    it 'should render new on failed save' do
+      new_admin = attributes_for(:invalid_admin)
       post :create, admin: new_admin
       response.should render_template('admins/new')
-   end 
+    end
   end
 
   context 'update' do
     it 'should successfully update admin' do
-      updated_admin = {name: 'pulkitko', contact_no: "8765432199", status: false}
+      updated_admin = attributes_for(:valid_admin_inactive, name: 'cooga')
       put :update, id: @sample_admin.id, admin: updated_admin
-      assigns(:info)[:contact_no].should == updated_admin[:contact_no]
+      controller.instance_variable_get(:@info)[:contact_no].should == updated_admin[:contact_no]
     end
 
     it 'should redirect to admins_path on successful update' do
-      updated_admin = {name: 'pulkitko', contact_no: "8765432199", status: false}
+      updated_admin = attributes_for(:valid_admin_inactive, name: 'cooga')
       put :update, id: @sample_admin.id, admin: updated_admin
       response.should redirect_to(admins_path)
     end
 
     it 'should render edit update failure' do
-      updated_admin = {name: 'pulkitko', contact_no: "87654321", status: false}
-      put :update, id: @sample_admin.id, admin: updated_admin
+      updated_invalid_admin = attributes_for(:valid_admin_active, contact_no: '8765432')
+      put :update, id: @sample_admin.id, admin: updated_invalid_admin
       response.should render_template('admins/edit')
     end
   end
 
   context 'destroy' do
     it 'should delete given admin' do
-      delete :destroy, id:@sample_admin.id
+      delete :destroy, id: @sample_admin.id
       Admin.all.should_not == @sample_admins
     end
     it 'should redirect to admins_path on successful delete.' do
-      delete :destroy, id:@sample_admin.id
+      delete :destroy, id: @sample_admin.id
       response.should redirect_to(admins_path)
-    end 
+    end
   end
 
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe Admin do
 
   before :each do
-    @admin = Admin.new name: "sample", contact_no: "1234567890", status: false
+    @admin = build(:valid_admin_active)
   end
 
-  context "Class structure validations" do
+  context 'Class structure validations' do
 
-    it "should have all required fields  " do
+    it 'should have all required fields' do
       (@admin.has_attribute? :name).should be_true
       (@admin.has_attribute? :contact_no).should be_true
       (@admin.has_attribute? :status).should be_true
@@ -16,76 +16,56 @@ describe Admin do
 
   end
 
-  context "all mandatory fields" do
+  context 'all mandatory fields' do
 
-    it "should contain a value" do
+    it 'should contain a value' do
       @admin.save.should be_true
     end
 
   end
 
-  context "admin name" do
+  context 'admin name' do
 
-    it "should not be blank" do
+    it 'should not be blank' do
       @admin.name = nil
       @admin.save.should be_false
     end
 
-    it "should not contain digits" do
-      @admin.name = "hello@123"
+    it 'should not contain digits' do
+      @admin.name = 'hello@123'
       @admin.save.should be_false
     end
 
   end
 
-  context "User contact number " do
+  context 'User contact number ' do
 
-    it "should not be blank" do
+    it 'should not be blank' do
       @admin.contact_no = nil
       @admin.save.should be_false
-      @admin.errors[:contact_no].first.should == "can't be blank"
+      @admin.errors[:contact_no].first.should == 'can\'t be blank'
     end
 
-    it "should not contain non-numeric data" do
-      @admin.contact_no = "a$$@7466748"
+    it 'should not contain non-numeric data' do
+      @admin.contact_no = 'a$$@7466748'
       @admin.save.should be_false
-      @admin.errors[:contact_no].first.should == "is not a number"
+      @admin.errors[:contact_no].first.should == 'is not a number'
     end
 
-    it "should not contain more or less than ten digits" do
-      @admin.contact_no = "45678"
+    it 'should not contain more or less than ten digits' do
+      @admin.contact_no = '45678'
       @admin.save.should be_false
-      @admin.errors[:contact_no].first.should == "is the wrong length (should be 10 characters)"
+      @admin.errors[:contact_no].first.should == 'is the wrong length (should be 10 characters)'
     end
 
   end
-  context "User Status " do
-    it "should not contain other than a boolean value " do
+
+  context 'User Status ' do
+    it 'should not contain other than a boolean value ' do
       @admin.status = nil
       @admin.save.should be_false
-      @admin.errors[:status].first.should == "is not included in the list"
+      @admin.errors[:status].first.should == 'is not included in the list'
     end
   end
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
