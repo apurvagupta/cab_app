eddf51a9c2a7de1c6ed3ccde147000e4d7fb64e7
Priyanka Pathak <ppathak@inppathak.local>
2013-04-05 13:37:57 +0530

    removed readonly from datepicker field

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -52,6 +52,8 @@ $(document).ready(function() {
         }
     });
 
+    $('.date_picker').keypress(function(event) {event.preventDefault();});
+    $('#pick_up_date').keypress(function(event) {event.preventDefault();});
     $('#pick_up_date').datepicker({dateFormat: "dd/mm/yy",minDate: 0});
     $('.date_picker').datepicker({dateFormat: "dd/mm/yy"});
     $("#pick_up_date").keypress(function(event) {event.preventDefault();});
