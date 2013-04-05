3395829bd45e495abf547baa1c2d4b5fcea040cf
Vishal Sharma <vishalsh@invishalsh.corporate.thoughtworks.com>
2013-04-05 13:35:23 +0530

    <vishal>header issue resolved

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -54,6 +54,7 @@ $(document).ready(function() {
 
     $('#pick_up_date').datepicker({dateFormat: "dd/mm/yy",minDate: 0});
     $('.date_picker').datepicker({dateFormat: "dd/mm/yy"});
+    $("#pick_up_date").keypress(function(event) {event.preventDefault();});
     $('#dropdown').click(function(){
         $("#dropdown").backgroundColor='orangered';
     });
