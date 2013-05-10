24079bbcd54c3d7b030164ddc185cbbcb23616bf
Riya Kataria <riyakata@inriyakata.corporate.thoughtworks.com>
2013-05-10 11:39:08 +0530

    <all> mail conigurations reset

---
+++ app/mailers/cab_request_mailer.rb
@@ -0,0 +1,20 @@
+class CabRequestMailer < ActionMailer::Base
+  default :from => "twcabrequest@gmail.com"
+
+# send a signup email to the user, pass in the user object that contains the user's email address
+  def send_email(cab_request)
+
+    mail(:to => "sharvishi9118@gmail.com",:subject => "Name: " + cab_request.traveler_name+
+
+        ",Contact_no: " + cab_request.contact_no+
+
+        ",From: " + cab_request.source+
+
+        ",To: " + cab_request.destination+
+
+        ",On: " + cab_request.pick_up_date_time.to_s+
+
+        ",No of passengers: " + cab_request.no_of_passengers.to_s )
+
+  end
+end
